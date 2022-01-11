b85676bac96309440c05c15cd04bbdcd0c5ff5da
karsyn ansari <karsyn.ansari@gmail.com>
2022-01-10 17:19:12 -0700

    commit tree 3cbe3ecde90cd9a7cd473465b2ef50e4eb5e81ac

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+// const num1 = 31;
+// const num2 = 2;
+// const multiply = num1 * num2;
 
-// Write your code here
+// const random = Math.floor(Math.random() * 10);
+// const num3 = 19;
+// const num4 = 5;
+// const mod = num3 % num4;
+
+// const max = Math.max(1, 2, 3, 20);
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10);
+
+let num3 = 19;
+let num4 = 5;
+
+const mod = num3 % num4;
+
+let max = Math.max(1, 2, 3, 19, 20);
